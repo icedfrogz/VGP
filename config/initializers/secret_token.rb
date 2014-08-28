6d6b513aa1594ba0bf689f4df6c53ea336feee11
icedfrogz <timewarp.rincon@gmail.com>
2014-08-27 20:04:05 -0700

    Initialized repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VgpApp::Application.config.secret_key_base = 'fb04b44da780c9fa6b227a16442489d8d5a5bf51ea1e614d22d0180b2dc255b6ffa67dc8275be0e62e6fbf5f9703f20cd3f378d415952b1edaced9460bbcac0b'
